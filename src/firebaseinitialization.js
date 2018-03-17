79e44f35fed56ff3c6d39d5a933035e71962704c
EidurOrn <eog26@hi.is>
2018-03-17 15:17:59 +0000

    Authentication has been implemented.
    Google account and email authentication has been added.
    Only users signed in can view the quiz management and they can only see their own created
    quizzes.
    
    Minor error: When forbidden access was implemented (for when someone tries to go to
    the management view without being signed in) it now always manages to
    redirect to the forbidden site instead of redirecting to the login site
    when the user signs out.

---
+++ src/firebaseinitialization.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyALJ6cVCitjh9VbVbBd2QBSE2HVn0vZz5o",
+authDomain: "pub-quiz-f19f3.firebaseapp.com",
+databaseURL: "https://pub-quiz-f19f3.firebaseio.com",
+projectId: "pub-quiz-f19f3",
+storageBucket: "pub-quiz-f19f3.appspot.com",
+messagingSenderId: "878829555843"
+};
+firebase.initializeApp(config);
--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@
  * Initializes the quiz
  * 
  * @author Ragnheiður Ásta Karlsdóttir rak4@hi.is
- * 14. march 2018
+ * @author Eiður Örn Gunnarsson eog26@hi.is
+ * 17. march 2018
  */
 const firebase = require('firebase');
 const questions = require('./questions');
 const manageQuiz = require('./manageQuiz');
 const quizOverview = require('./quizOverview');
 const newQuiz = require('./newQuiz');
-const username = 'admin';
 
 // Initialize Firebase
 const config = {
@@ -20,6 +20,35 @@ const config = {
 };
 firebase.initializeApp(config);
 const database = firebase.database();
+
+// Handles what to do when a user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user){
+  // When signed in.
+  if(user){
+    var el = document.querySelector('#loggedIn');
+
+    const welcomeUser = document.createElement('p');
+    if(user.displayName != null) welcomeUser.appendChild(document.createTextNode("Velkomin(n): "+user.displayName));
+    else welcomeUser.appendChild(document.createTextNode("Skráður inn notandi er: "+user.email))
+    el.appendChild(welcomeUser);
+
+    const logoutButton = document.createElement('button');
+    logoutButton.addEventListener('click', () => {
+      logout();
+    });
+    logoutButton.appendChild(document.createTextNode("Útskráning"));
+    el.appendChild(logoutButton);
+
+    console.log(user.displayName+" is signed in.");
+
+    getQuizzes(user.uid);
+  }
+  // When not signed in.
+  else{
+    console.log("No user signed in.");
+    window.location.replace("forbidden.html");
+  }
+});
 
 // Fires up quizzes
 function initializeQuiz(quizName) {
@@ -34,27 +63,39 @@ function listenToWhichQuiz(quizzes) {
   while (navbar.firstChild) {
     navbar.removeChild(navbar.firstChild);
   }
-  Object.keys(quizzes).forEach((quizName) => {
-    const button = document.createElement('button');
-    button.addEventListener('click', () => {
-      initializeQuiz(quizName);
+  if(quizzes != null){
+    Object.keys(quizzes).forEach((quizName) => {
+      const button = document.createElement('button');
+      button.addEventListener('click', () => {
+        initializeQuiz(quizName);
+      });
+      button.appendChild(document.createTextNode(quizName));
+      navbar.appendChild(button);
     });
-    button.appendChild(document.createTextNode(quizName));
-    navbar.appendChild(button);
-  });
+  }
+  
   const button = document.createElement('button');
   button.addEventListener('click', () => {
-    newQuiz.init(database, initializeQuiz, username);
+    var user = firebase.auth().currentUser;
+    newQuiz.init(database, initializeQuiz, user.uid);
   });
   button.appendChild(document.createTextNode('Nýtt quiz'));
   navbar.appendChild(button);
 }
 
 // Updates teams and quizzes
-document.addEventListener('DOMContentLoaded', () => {
+function getQuizzes(username){
   // Get a reference to the database service
   const quizzesRef = database.ref(`/hosts/${username}/quizzes`);
   quizzesRef.on('value', (snapshot) => {
     listenToWhichQuiz(snapshot.val());
   });
-});
+};
+
+function logout(){
+  firebase.auth().signOut().then(function() {
+    window.location.replace("./");
+  }).catch(function(error) {
+    console.log("Sign out failed");
+  });
+}
